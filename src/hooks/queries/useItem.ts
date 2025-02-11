d06270efb3c3a49f296fa6b80c1a03ac6432b8e2
Dian Chun Choy <choydianchun@gmail.com>
2025-02-11 22:32:56 +0800

    add all crud to useItem hook

--- src/hooks/queries/useItem.ts
+++ src/hooks/queries/useItem.ts
@@ -1,10 +1,10 @@
 import { useAuth } from '@clerk/clerk-react'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { Item } from '@/@types/items'
 import { vaultApi } from '@/services'
 import { QUERY_KEYS } from '@/utils/constants/queryKeys'
 
-const url = '/items'
+const path = '/items'
 
 const useItem = () => {
   const { getToken } = useAuth()
@@ -13,7 +13,51 @@ const useItem = () => {
     useQuery({
       queryKey: QUERY_KEYS.GET_ITEMS,
       queryFn: async (): Promise<Array<Item>> => {
-        const res = await vaultApi.get(url, {
+        const res = await vaultApi.get(path, {
+          headers: {
+            Authorization: `Bearer ${await getToken()}`,
+          },
+        })
+        return res.data
+      },
+    })
+
+  const useCreateItemMutation = () =>
+    useMutation({
+      mutationFn: async (name: string): Promise<Item> => {
+        const res = await vaultApi.post(
+          path,
+          { name },
+          {
+            headers: {
+              Authorization: `Bearer ${await getToken()}`,
+            },
+          }
+        )
+        return res.data
+      },
+    })
+
+  const useUpdateItemMutation = () =>
+    useMutation({
+      mutationFn: async (request: Item): Promise<Item> => {
+        const res = await vaultApi.put(
+          `${path}/${request.id}`,
+          { name: request.name },
+          {
+            headers: {
+              Authorization: `Bearer ${await getToken()}`,
+            },
+          }
+        )
+        return res.data
+      },
+    })
+
+  const useDeleteItemMutation = () =>
+    useMutation({
+      mutationFn: async (id: number): Promise<null> => {
+        const res = await vaultApi.delete(`${path}/${id}`, {
           headers: {
             Authorization: `Bearer ${await getToken()}`,
           },
@@ -23,7 +67,10 @@ const useItem = () => {
     })
 
   return {
+    useCreateItemMutation,
+    useDeleteItemMutation,
     useGetItemsQuery,
+    useUpdateItemMutation,
   }
 }
 
