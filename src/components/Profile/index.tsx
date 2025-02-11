7e6038167a5b35efef34b186bf9ea01f95466721
Dian Chun Choy <choydianchun@gmail.com>
2025-02-11 16:26:11 +0800

    add loader to profile and logout

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,29 +1,36 @@
 import { SignOutButton, useUser } from '@clerk/clerk-react'
 import { MdLogout } from 'react-icons/md'
+import { RiLoader4Line } from 'react-icons/ri'
 import { Avatar, Page } from '@/components/commons'
 
 const Profile = () => {
-  const { user } = useUser()
+  const { user, isLoaded } = useUser()
 
   return (
     <Page>
       <div className='flex h-full w-full flex-col items-center bg-white px-6 py-4'>
-        <Avatar
-          url={user?.imageUrl}
-          email={user?.primaryEmailAddress?.emailAddress}
-          className='mt-16 mb-3'
-        />
+        {isLoaded ? (
+          <>
+            <Avatar
+              url={user?.imageUrl}
+              email={user?.primaryEmailAddress?.emailAddress}
+              className='mt-16 mb-3'
+            />
 
-        <p className='text-app-default mb-9 text-lg font-semibold'>
-          {user?.primaryEmailAddress?.emailAddress}
-        </p>
+            <p className='text-app-default mb-9 text-lg font-semibold'>
+              {user?.primaryEmailAddress?.emailAddress}
+            </p>
 
-        <SignOutButton>
-          <button className='text-app-default bg-app-background flex w-full items-center gap-x-3 rounded-md p-4'>
-            <MdLogout className='h-5 w-5' />
-            Logout
-          </button>
-        </SignOutButton>
+            <SignOutButton>
+              <button className='text-app-default bg-app-background flex w-full items-center gap-x-3 rounded-md p-4'>
+                <MdLogout className='h-5 w-5' />
+                Logout
+              </button>
+            </SignOutButton>
+          </>
+        ) : (
+          <RiLoader4Line className='text-app-secondary my-auto h-10 w-10 animate-spin' />
+        )}
       </div>
     </Page>
   )
