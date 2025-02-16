6c2324c3d7e3b62fa213db5b2a72868a74fc68ec
Dian Chun Choy <choydianchun@gmail.com>
2025-02-16 16:17:59 +0800

    refactor generate password logic

--- src/components/Item/GeneratePassword/index.tsx
+++ src/components/Item/GeneratePassword/index.tsx
@@ -1,53 +1,91 @@
-import { MouseEvent, useState } from 'react'
-import { GeneratePassword as generatePw } from 'generate-password-lite'
+import { ChangeEvent, MouseEvent, useState } from 'react'
+import { GeneratePassword as generatePasswordLite } from 'generate-password-lite'
 import { BsCopy } from 'react-icons/bs'
 import { Button, Input, Menu, Switch } from '@/components/commons'
 import { ButtonVariant } from '@/components/commons/Button/types'
+import { useToastContext } from '@/contexts/useToastContext/context'
+
+type Settings = {
+  numbers: boolean
+  symbols: boolean
+  length: string
+}
+
+const defaultSettings: Settings = {
+  numbers: true,
+  symbols: true,
+  length: '10',
+} as const
+
+const permanentSettings = {
+  minLengthLowercase: 0,
+  minLengthUppercase: 0,
+  minLengthNumbers: 0,
+}
 
 const GeneratePassword = () => {
-  const [isVisible, setIsVisible] = useState(false)
-  const [isNumbers, setIsNumbers] = useState(true)
-  const [isSymbols, setIsSymbols] = useState(true)
-  const [length, setLength] = useState('10')
-  const [password, setPassword] = useState(generatePassword())
+  const { toast } = useToastContext()
+  const [isMenuVisible, setIsMenuVisible] = useState(false)
+  const [settings, setSettings] = useState<Settings>({ ...defaultSettings })
+  const [passwordDisplay, setPasswordDisplay] = useState(
+    generatePassword(defaultSettings)
+  )
 
-  function generatePassword() {
+  function generatePassword(config: Settings) {
     try {
-      const pw = generatePw({
-        length: parseInt(length),
-        symbols: isSymbols,
-        numbers: isNumbers,
-        minLengthLowercase: 0,
-        minLengthUppercase: 0,
-        minLengthNumbers: 0,
+      const length = parseInt(config.length)
+      if (!length) {
+        throw new Error('Invalid password length input.')
+      }
+      const password = generatePasswordLite({
+        numbers: config.numbers,
+        symbols: config.symbols,
+        length: length,
+        ...permanentSettings,
       })
-      return pw
+      return password
     } catch (e) {
-      // TODO:
-      console.log(e)
+      toast.error(`${e}`)
     }
   }
 
-  const handleCloseMenu = () => {
-    // TODO: Reset
-    setIsVisible(false)
+  const closeMenu = () => {
+    setSettings(defaultSettings)
+    setPasswordDisplay(generatePassword(defaultSettings))
+    setIsMenuVisible(false)
   }
 
-  const handleOpenMenu = (e: MouseEvent<HTMLButtonElement>) => {
+  const openMenu = (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
-    setIsVisible(true)
+    setIsMenuVisible(true)
+  }
+
+  const handleChangeLength = (e: ChangeEvent<HTMLInputElement>) =>
+    setSettings((prev) => ({ ...prev, length: e.target.value }))
+
+  const handleChangeNumbers = () =>
+    setSettings((prev) => ({ ...prev, numbers: !prev.numbers }))
+
+  const handleChangeSymbols = () =>
+    setSettings((prev) => ({ ...prev, symbols: !prev.symbols }))
+
+  const regeneratePassword = () => {
+    const newPassword = generatePassword(settings)
+    if (newPassword) {
+      setPasswordDisplay(newPassword)
+    }
   }
 
   return (
     <>
       <Menu
-        isVisible={isVisible}
-        onClose={handleCloseMenu}
+        isVisible={isMenuVisible}
+        onClose={closeMenu}
         footer={
           <div className='flex items-center justify-between px-6 pt-3 pb-6'>
             <Button
               variant={ButtonVariant.OUTLINE}
-              onClick={() => setPassword(generatePassword())}
+              onClick={regeneratePassword}
             >
               Re-generate
             </Button>
@@ -58,8 +96,8 @@ const GeneratePassword = () => {
         <div className='mt-6 grid grid-cols-[1fr_auto] items-center gap-x-4'>
           <Input
             className='bg-app-background'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            value={passwordDisplay}
+            onChange={(e) => setPasswordDisplay(e.target.value)}
           />
           <BsCopy className='h-6 w-6 text-slate-500' />
         </div>
@@ -71,29 +109,23 @@ const GeneratePassword = () => {
           <Input
             className='bg-app-background w-[50px] p-1.5 text-sm'
             type='number'
-            value={length}
-            onChange={(e) => setLength(e.target.value)}
+            value={settings.length}
+            onChange={handleChangeLength}
           />
         </div>
 
         <div className='flex h-[50px] items-center justify-between'>
           <p className='text-app-default font-semibold'>Numbers</p>
-          <Switch
-            checked={isNumbers}
-            onChange={() => setIsNumbers((prev) => !prev)}
-          />
+          <Switch checked={settings.numbers} onChange={handleChangeNumbers} />
         </div>
 
         <div className='flex h-[50px] items-center justify-between'>
           <p className='text-app-default font-semibold'>Symbols</p>
-          <Switch
-            checked={isSymbols}
-            onChange={() => setIsSymbols((prev) => !prev)}
-          />
+          <Switch checked={settings.symbols} onChange={handleChangeSymbols} />
         </div>
       </Menu>
 
-      <Button className='mt-3 w-full' onClick={handleOpenMenu}>
+      <Button className='mt-3 w-full' onClick={openMenu}>
         Generate
       </Button>
     </>
--- src/components/commons/Toast/index.tsx
+++ src/components/commons/Toast/index.tsx
@@ -6,7 +6,7 @@ const Toast = () => {
   const { toasts, toast } = useToastContext()
 
   return (
-    <div className='absolute top-0 flex w-full justify-center'>
+    <div className='fixed top-0 z-10 flex w-full justify-center'>
       {toasts.length > 0 && (
         <div className='font-noto-sans flex w-md max-w-md flex-col gap-y-2 p-6'>
           <AnimatePresence>
