8c06de4363e091488ebc67c7ffef0a496e697469
Dian Chun Choy <choydianchun@gmail.com>
2025-03-03 12:18:43 +0800

    add open delete cfm modal to swipe x trigger

--- src/components/Main/Dashboard/ItemTile/index.tsx
+++ src/components/Main/Dashboard/ItemTile/index.tsx
@@ -1,9 +1,10 @@
-import { MouseEvent } from 'react'
+import { MouseEvent, useRef, useState } from 'react'
 import { DateTime } from 'luxon'
 import { FiEdit } from 'react-icons/fi'
 import { useNavigate } from 'react-router'
 import { Item } from '@/@types/items'
-import { SwipeX } from '@/components/commons'
+import { ModalConfirmDelete, SwipeX } from '@/components/commons'
+import { SwipeXRef } from '@/components/commons/SwipeX'
 import { CategoryColor, Route } from '@/utils/constants/enums'
 
 type ItemTileProps = {
@@ -19,42 +20,67 @@ const ItemTile = ({
   categoryInitials,
   onEdit,
 }: ItemTileProps) => {
+  const [isDeleteConfirmationVisible, setIsDeleteConfirmationVisible] =
+    useState(false)
   const navigate = useNavigate()
+  const swipeRef = useRef<SwipeXRef>(null)
 
   const handleEdit = (e: MouseEvent<HTMLButtonElement>) => {
     e.stopPropagation()
     onEdit()
   }
 
+  const handleCancelDelete = () => {
+    setIsDeleteConfirmationVisible(false)
+    swipeRef.current?.reset()
+  }
+
   const handleClickTile = () =>
     navigate(`${Route.ITEMS}/${btoa(item.id.toString())}`)
 
   return (
-    <SwipeX onClick={handleClickTile}>
-      <div className='grid h-[80px] grid-cols-[auto_1fr_auto] items-center gap-x-3 rounded-md bg-white p-4 text-left hover:cursor-pointer'>
-        <div
-          className='flex h-12 w-12 items-center justify-center rounded-full text-xl text-white'
-          style={{
-            backgroundColor: categoryColor,
-          }}
-        >
-          {categoryInitials}
-        </div>
+    <>
+      <SwipeX
+        onClick={handleClickTile}
+        onTrigger={() => setIsDeleteConfirmationVisible(true)}
+        onRevertTrigger={() => setIsDeleteConfirmationVisible(false)}
+        ref={swipeRef}
+      >
+        <div className='grid h-[80px] grid-cols-[auto_1fr_auto] items-center gap-x-3 rounded-md bg-white p-4 text-left hover:cursor-pointer'>
+          <div
+            className='flex h-12 w-12 items-center justify-center rounded-full text-xl text-white'
+            style={{
+              backgroundColor: categoryColor,
+            }}
+          >
+            {categoryInitials}
+          </div>
 
-        <div className='flex w-0 min-w-full flex-col gap-y-0.5'>
-          <p className='text-app-default truncate font-semibold'>{item.name}</p>
-          <p className='truncate text-sm text-slate-500'>
-            {DateTime.fromJSDate(new Date(item.created_at)).toFormat(
-              'd MMM yyyy'
-            )}
-          </p>
-        </div>
+          <div className='flex w-0 min-w-full flex-col gap-y-0.5'>
+            <p className='text-app-default truncate font-semibold'>
+              {item.name}
+            </p>
+            <p className='truncate text-sm text-slate-500'>
+              {DateTime.fromJSDate(new Date(item.created_at)).toFormat(
+                'd MMM yyyy'
+              )}
+            </p>
+          </div>
 
-        <button onClick={handleEdit} className='hover:cursor-pointer'>
-          <FiEdit className='h-6 w-6 text-slate-500' />
-        </button>
-      </div>
-    </SwipeX>
+          <button onClick={handleEdit} className='hover:cursor-pointer'>
+            <FiEdit className='h-6 w-6 text-slate-500' />
+          </button>
+        </div>
+      </SwipeX>
+
+      <ModalConfirmDelete
+        isVisible={isDeleteConfirmationVisible}
+        onConfirm={() => {}}
+        onClose={handleCancelDelete}
+        text=''
+        // isLoading
+      />
+    </>
   )
 }
 
--- src/components/commons/SwipeX/index.tsx
+++ src/components/commons/SwipeX/index.tsx
@@ -1,93 +1,136 @@
-import { MouseEvent, PropsWithChildren, TouchEvent, useState } from 'react'
+import {
+  ComponentProps,
+  forwardRef,
+  MouseEvent,
+  PropsWithChildren,
+  TouchEvent,
+  useImperativeHandle,
+  useState,
+} from 'react'
 import { BsTrash } from 'react-icons/bs'
 import { mc } from '@/utils/functions/commons'
 
 const swipeLimit = 140
 
+export type SwipeXRef = {
+  reset: () => void
+}
+
 type SwipeXProps = {
   onClick: () => void
-} & PropsWithChildren
+  onTrigger: () => void
+  onRevertTrigger: () => void
+} & PropsWithChildren &
+  ComponentProps<'div'>
 
-const SwipeX = ({ onClick, children }: SwipeXProps) => {
-  const [initial, setInitial] = useState(0)
-  const [delta, setDelta] = useState(0)
-  const [prevX, setPrevX] = useState(0)
-  const [isTriggered, setIsTriggered] = useState(false)
+const SwipeX = forwardRef<SwipeXRef, SwipeXProps>(
+  ({ onClick, children, onTrigger, onRevertTrigger }, ref) => {
+    const [initial, setInitial] = useState(0)
+    const [delta, setDelta] = useState(0)
+    const [prevX, setPrevX] = useState(0)
+    const [isTriggered, setIsTriggered] = useState(false)
 
-  const handleMouseUp = (e: MouseEvent<HTMLDivElement>) => {
-    if (e.pageX === initial) {
-      onClick()
-      return
-    }
-    setPrevX(delta)
-  }
+    const isEditButton = (e: Element) =>
+      (e as Element).matches('button') ||
+      (e as Element).matches('svg') ||
+      (e as Element).matches('path')
 
-  const handleTouchEnd = (e: TouchEvent<HTMLDivElement>) => {
-    if (e.changedTouches[0].clientX === initial) {
-      onClick()
-      return
-    }
-    setPrevX(delta)
-  }
-
-  const handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
-    if (e.buttons !== 1) return
-    handleMove(e.pageX)
-  }
-
-  const handleMove = (currentX: number) => {
-    let delta = initial - currentX
-    delta = delta * -1
-    delta = delta + prevX
-
-    if (delta > 0) {
-      delta = 0
+    const handleMouseUp = (e: MouseEvent<HTMLDivElement>) => {
+      if (isEditButton(e.target as Element)) {
+        return
+      }
+      if (e.pageX === initial) {
+        onClick()
+        return
+      }
+      setPrevX(delta)
     }
 
-    if (delta < -swipeLimit) {
-      delta = -500
-      setIsTriggered(true)
-    } else if (isTriggered) {
-      setIsTriggered(false)
+    const handleTouchEnd = (e: TouchEvent<HTMLDivElement>) => {
+      if (isEditButton(e.target as Element)) {
+        return
+      }
+      if (e.changedTouches[0].clientX === initial) {
+        onClick()
+        return
+      }
+      setPrevX(delta)
     }
 
-    setDelta(delta)
-  }
+    const handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
+      if (e.buttons !== 1) return
+      handleMove(e.pageX)
+    }
 
-  return (
-    <div className='grid-stack min-h-[80px] w-full overflow-hidden rounded-md'>
-      <div className='bg-app-danger relative flex items-center justify-end rounded-md'>
-        <div className='z-10 flex items-center gap-x-2 pr-6 text-sm text-white'>
-          <BsTrash className='h-5 w-5' />
-          Delete
+    const handleMove = (currentX: number) => {
+      let delta = initial - currentX
+      delta = delta * -1
+      delta = delta + prevX
+
+      if (delta > 0) {
+        delta = 0
+      }
+
+      if (delta < -swipeLimit) {
+        delta = -500
+        setIsTriggered(true)
+        onTrigger()
+      } else if (isTriggered) {
+        setIsTriggered(false)
+        onRevertTrigger()
+      }
+
+      setDelta(delta)
+    }
+
+    useImperativeHandle(
+      ref,
+      () => ({
+        reset: () => {
+          setInitial(0)
+          setDelta(0)
+          setIsTriggered(false)
+          setPrevX(0)
+        },
+      }),
+      []
+    )
+
+    return (
+      <div className='grid-stack min-h-[80px] w-full overflow-hidden rounded-md'>
+        <div className='bg-app-danger relative flex items-center justify-end rounded-md'>
+          <div className='z-10 flex items-center gap-x-2 pr-6 text-sm text-white'>
+            <BsTrash className='h-5 w-5' />
+            Delete
+          </div>
+          <div
+            className={mc(
+              'absolute right-12 h-[1px] w-[1px] rounded-full bg-slate-800 transition-[scale] duration-250',
+              isTriggered && 'scale-[100000%]'
+            )}
+          />
         </div>
-        <div
-          className={mc(
-            'absolute right-12 h-[1px] w-[1px] rounded-full bg-slate-800 transition-[scale] duration-250',
-            isTriggered && 'scale-[100000%]'
-          )}
-        />
-      </div>
 
-      <div
-        onMouseDown={(e) => setInitial(e.pageX)}
-        onMouseMove={handleMouseMove}
-        onMouseUp={handleMouseUp}
-        onTouchStart={(e) => setInitial(e.changedTouches[0].clientX)}
-        onTouchMove={(e) => handleMove(e.changedTouches[0].clientX)}
-        onTouchEnd={handleTouchEnd}
-        className={mc(
-          'rounded-md bg-white',
-          isTriggered && 'transition-transform duration-700'
-        )}
-        style={{
-          transform: `translateX(${delta}px)`,
-        }}
-      >
-        {children}
+        <div
+          onMouseDown={(e) => setInitial(e.pageX)}
+          onMouseMove={handleMouseMove}
+          onMouseUp={handleMouseUp}
+          onTouchStart={(e) => setInitial(e.changedTouches[0].clientX)}
+          onTouchMove={(e) => handleMove(e.changedTouches[0].clientX)}
+          onTouchEnd={handleTouchEnd}
+          className={mc(
+            'rounded-md bg-white',
+            isTriggered && 'transition-transform duration-700'
+          )}
+          style={{
+            transform: `translateX(${delta}px)`,
+          }}
+        >
+          {children}
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
+)
 
 export default SwipeX
